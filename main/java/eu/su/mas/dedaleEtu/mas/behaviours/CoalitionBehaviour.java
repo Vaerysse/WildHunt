189a68af361eb237e087441d85a3298d511c4b2e
Miette <laura.xenard@etu.parisdescartes.fr>
2020-03-19 19:21:04 +0100

    Vague début de CoalitionBehaviour

---
+++ main/java/eu/su/mas/dedaleEtu/mas/behaviours/CoalitionBehaviour.java
@@ -0,0 +1,227 @@
+package eu.su.mas.dedaleEtu.mas.behaviours;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import dataStructures.serializableGraph.SerializableSimpleGraph;
+import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import eu.su.mas.dedaleEtu.mas.agents.dummies.ExploreSoloAgent;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation.MapAttribute;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+
+public class CoalitionBehaviour extends SimpleBehaviour{
+
+	private static final long serialVersionUID = 9088209402507795292L;
+	private static final int wait = 2000;
+	
+	private boolean finished;
+	
+	/**
+	 * To control the incoming communication flow
+	 */
+	private boolean connection, exchange, ACKmap, sendProposal, sendAnswer, sendConfirmation; // sendConnection, sendMap, sendACKmap
+	
+	/**
+	 * To control the outgoing communication flow
+	 */
+	private int stepProtocol;
+	
+	/**
+	 * Name of the agent to communicate with
+	 */
+	private String receiverName;
+	
+	/**
+	 * Current knowledge of the agent regarding the environment
+	 */
+	private MapRepresentation myMap;
+	
+	
+	private long timer;
+	
+	/**
+	 * 
+	 * Private communication channel to exchange information between 2 agents.
+	 * @param myagent
+	 * @param receiverName The local name of the agent myagent tries to communicate with
+	 */
+	public CoalitionBehaviour(final Agent myagent, String receiverName) {
+		super(myagent);
+		
+		this.myMap = ((ExploreSoloAgent)this.myAgent).getMap();
+		this.receiverName = receiverName;
+		this.finished = false;
+		this.connection = true;
+		this.sendProposal = true;
+		this.exchange = false;
+		this.sendAnswer = false;
+		this.ACKmap = false;
+		this.sendConfirmation = false;
+		this.stepProtocol = 1;
+		this.timer = System.currentTimeMillis();
+		//ajouter un timer pour éviter les attentes à l'infini
+		
+	}
+	
+	public void action() {
+		
+		// Sending messages
+		if (this.sendProposal){
+			// Sending a message to propose a coalition
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.setSender(this.myAgent.getAID());
+			msg.setProtocol("CoalitionProtocol");
+			msg.setContent("coalition proposal");
+			msg.addReceiver(new AID(this.receiverName, AID.ISLOCALNAME));
+			((AbstractDedaleAgent)this.myAgent).sendMessage(msg);
+			System.out.println(this.myAgent.getLocalName() + ": I want to create a coalition with " + this.receiverName);
+		}
+		else if (this.sendAnswer) {
+			
+			// TODO: déterminer si intéressant d'entrer ds la coalition ou pas
+			String answer = new String(); // acceptation or refusal
+			
+			// Sending an answer to the coalition proposal
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.setSender(this.myAgent.getAID());
+			msg.setProtocol("CoalitionProtocol");
+			
+			// TODO: déterminer si on répond à tous les agents de la proposition de coalition
+			msg.addReceiver(new AID(this.receiverName, AID.ISLOCALNAME));
+			
+			msg.setContent(answer);
+			((AbstractDedaleAgent)this.myAgent).sendMessage(msg);
+			System.out.println(this.myAgent.getLocalName() + ": I've sent my answer ("+ answer +") to " + this.receiverName);
+		}
+		else if (this.sendConfirmation) {
+			
+			// TODO: déterminer si on confirme la coalition ou non
+			String answer = new String();
+			// on met la confirmation à ce niveau là ?
+			
+			// Confirming the coalition creation
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.setSender(this.myAgent.getAID());
+			msg.setProtocol("CoalitionProtocol");
+			msg.setContent(answer);
+			
+			// TODO: idem, transmettre au chef ou à tous les agents ?
+			msg.addReceiver(new AID(this.receiverName,AID.ISLOCALNAME));
+			((AbstractDedaleAgent)this.myAgent).sendMessage(msg);
+			System.out.println(this.myAgent.getLocalName() + ": I've sent a map ACK to " + this.receiverName);
+		}
+
+		/*
+		// Receiving messages
+		if (this.connection) {
+			// Receiving a request for the opening of a private channel
+			MessageTemplate msgTemplate = MessageTemplate.and(MessageTemplate.and(
+										  MessageTemplate.MatchPerformative(ACLMessage.INFORM), 
+										  MessageTemplate.MatchSender(new AID(this.receiverName, AID.ISLOCALNAME))),
+										  MessageTemplate.MatchProtocol("ExchangeProtocol"));			
+			ACLMessage msgReceived = this.myAgent.receive(msgTemplate);
+			System.out.println("En attente de récéption message de demande de com priver");
+			if (msgReceived != null) {
+				System.out.println(this.myAgent.getLocalName() + ": I've received a request for a private channel from " + this.receiverName);
+				if (msgReceived.getContent().equals("connection")) {
+					this.connection = false;
+					this.exchange = true;
+					this.sendConnection = false;
+					this.sendMap = true;
+					System.out.println(this.myAgent.getLocalName() + ": I've opened a private channel with " + this.receiverName);
+				}
+				else if (msgReceived.getContent() == "-1" ) {
+					System.out.println(this.myAgent.getLocalName() + ": I have a connection problem with " + this.receiverName);
+					this.finished = true;
+				}
+			}
+			else {
+				if(System.currentTimeMillis() - this.timer >= wait){
+					((ExploreSoloAgent)this.myAgent).setMoving(true);
+					this.finished = true;
+				}
+			}
+		}
+		else if (this.exchange) {
+			// Receiving a map representation
+			MessageTemplate msgTemplate = MessageTemplate.and(MessageTemplate.and(
+										  MessageTemplate.MatchPerformative(ACLMessage.INFORM), 
+										  MessageTemplate.MatchSender(new AID(this.receiverName, AID.ISLOCALNAME))),
+										  MessageTemplate.MatchProtocol("MapProtocol"));			
+
+			ACLMessage msgReceived = this.myAgent.receive(msgTemplate);
+			System.out.println(this.myAgent.getLocalName() + ": I've received a map from " + this.receiverName);
+			this.exchange = false;
+			this.stepProtocol += 1;
+			this.ACKmap = true;
+			this.sendACKmap = true;
+			this.sendMap = false;
+			
+			// TODO: ligne debug pr print Map es-tu là ?
+			((ExploreSoloAgent)this.myAgent).getMap();
+			
+			if (msgReceived != null) {
+				if (msgReceived.getContent() == "-1" ) {
+					System.out.println(this.myAgent.getLocalName() + " - MAP RECEPTION PROBLEM");
+					this.finished = true;
+				}
+				else {
+					try {
+						System.out.println(msgReceived.getContentObject());
+						this.myMap.mergeMapData((HashMap<String, HashMap<String, ArrayList<String>>>) msgReceived.getContentObject());
+					} catch (UnreadableException e) {
+						System.out.println(this.myAgent.getLocalName() + " - MAP RECEPTION PROBLEM");
+						System.out.println(this.myAgent.getLocalName() + "dans l'ouverture de map");
+						e.printStackTrace();
+						this.finished = true;
+					}
+				
+				}
+			}
+			else {
+				if(System.currentTimeMillis() - this.timer >= wait){
+					((ExploreSoloAgent)this.myAgent).setMoving(true);
+					this.finished = true;
+				}
+			}
+		}
+		else if (this.ACKmap && this.sendACKmap) {
+			// Receiving an ACK for the map reception
+			MessageTemplate msgTemplate = MessageTemplate.and(MessageTemplate.and(
+										  MessageTemplate.MatchPerformative(ACLMessage.INFORM), 
+										  MessageTemplate.MatchSender(new AID(this.receiverName, AID.ISLOCALNAME))),
+										  MessageTemplate.MatchProtocol("ExchangeProtocol"));			
+
+			ACLMessage msgReceived = this.myAgent.receive(msgTemplate);
+			if (msgReceived != null) {
+				if (msgReceived.getContent().equals("ACKmap") ) {
+					System.out.println(this.myAgent.getLocalName() + ": I've received an ACK map from " + this.receiverName);
+					this.finished = true;
+					((ExploreSoloAgent)this.myAgent).setMoving(true);
+				}
+				else if (msgReceived.getContent() == "-1" ) {
+					System.out.println("ACK map reception problem");
+				}
+			}
+			else {
+				if(System.currentTimeMillis() - this.timer >= wait){
+					((ExploreSoloAgent)this.myAgent).setMoving(true);
+					this.finished = true;
+				}
+			}
+		}
+	*/	
+	}
+	
+	
+	public boolean done() {
+		return finished;
+	}
+}
